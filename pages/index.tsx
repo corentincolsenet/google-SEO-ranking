d54692e3abd081f5bbe37a27a81a17d251223350
Corentin Colsenet <corentincolsenet@Corentins-MacBook-Pro.local>
2022-10-04 11:22:31 +0200

    update description home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
       title="Xeofood | The first food delivery company in Xeozrodel"
       description="We offer the best food delivery in all Xeozrodel! 
       Get your best Xeozrodel food from neighbour restaurants right to your door. 
-      Recommended at 97% by xeozrodadvisor and reviewed at 5/5 by &apos;xeozrodel fork&apos;, the prestigious newspaper."
+      Recommended at 97% by xeozrodadvisor and reviewed at 5/5 by xeozrodel fork, the prestigious newspaper."
     />
     <div className="flex h-screen items-center justify-center">
       Let's win this competition 😎 🎉
