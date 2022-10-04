3d0455fbcb2203cd7390b5fd26b5f9b8618f2c3d
Corentin Colsenet <corentincolsenet@Corentins-MacBook-Pro.local>
2022-10-04 11:14:53 +0200

    add next-seo + add title and description for the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from 'next'
+import { NextSeo } from 'next-seo'
 
 const Home: NextPage = () => {
   return (
+    <>
+    <NextSeo
+      title="Xeofood | The first food delivery company in Xeozrodel"
+      description="We offer the best food delivery in all Xeozrodel! Get your best Xeozrodel food from neighbour restaurants right to your door. Recommended at 97% by xeozrodadvisor and reviewed at 5/5 by 'xeozrodel fork', the prestigious newspaper."
+    />
     <div className="flex h-screen items-center justify-center">
       Let's win this competition 😎 🎉
     </div>
+    </>
   )
 }
 
