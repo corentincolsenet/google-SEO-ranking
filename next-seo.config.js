f77d4faf6bb8f8268cb95052ccf0cd8054eb621d
LeoHnvl <leo.henneville@epitech.eu>
2022-11-04 21:27:12 +0100

    Next-seo config

---
+++ next-seo.config.js
@@ -0,0 +1,8 @@
+export default {
+    openGraph: {
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://www.xeozrodel-food.com/',
+        siteName: 'Xeofood',
+    }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 import Homepage from '../components/Home'
+import SEO from '../next-seo.config'
 
 import Layout from '../components/Layout'
 
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       description="We offer the best food delivery in all Xeozrodel! 
       Get your best Xeozrodel food from neighbour restaurants right to your door. 
       Recommended at 97% by xeozrodadvisor and reviewed at 5/5 by 'xeozrodel fork', the prestigious newspaper."
+      {...SEO}
     />
     <Layout>
         <Homepage/>
