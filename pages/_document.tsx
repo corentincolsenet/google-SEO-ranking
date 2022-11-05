99c53dbc5d9792960df8cf8cc7d3f795e709357b
Corentin Colsenet <corentincolsenet@Corentins-MacBook-Pro.local>
2022-11-05 11:15:35 +0100

    CC: fix iframe

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,7 @@ export default function Document() {
         <NextScript />
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MK8TXV5" height="0" width="0" style="display:none;visibility:hidden" />`,
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MK8TXV5" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
       </body>
