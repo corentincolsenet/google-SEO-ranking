df9b625e6511053f49ad1fe7ce497cda2bb60610
Corentin Colsenet <corentincolsenet@Corentins-MacBook-Pro.local>
2022-12-16 14:15:20 +0100

    CC: add id to the script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <Script
+          id='gtm'
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
